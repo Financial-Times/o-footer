7c72d94cf6b00cb14cdaaa46165b6d941a873343
Alberto Elias <me@alberto-elias.com>
2015-11-18 13:49:47 +0000

    Another demo fix

--- demos/src/config.js
+++ demos/src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	options: {
-		sass: 'demos/src/demos.scss',
+		sass: 'demos/src/demo.scss',
 		data: {
 			'o-ft-footer': JSON.parse(require('fs').readFileSync(process.cwd() + '/footer.json', {encoding: 'utf8'}))
 		}
